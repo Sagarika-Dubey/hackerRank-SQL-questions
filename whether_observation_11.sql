0fd2dab4539d0e0c809a3fc3e799784cafdd7d5d
Sagarika-Dubey <sagarikardubey@gmail.com>
2023-11-25 20:00:38 +0530

    whether observation 11

---
+++ whether_observation_11.sql
@@ -0,0 +1,1 @@
+select distinct CITY from STATION where not ( CITY like 'A%' or CITY like 'E%' or CITY like 'I%' or CITY like 'O%' or CITY like 'U%') or not( CITY like '%a' or CITY like '%e' or CITY like '%i' or CITY like '%o' or CITY like '%u');
